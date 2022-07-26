7e9c43b9e2cc795abdd6401b3d8cf0e9091f504b
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-07-26 22:28:00 +0900

    init:JDBC test

--- boilerplate-egovframe-xml/src/test/java/io/leedokchidok/boilerplate/JDBC/JDBCConnection.java
+++ boilerplate-egovframe-xml/src/test/java/io/leedokchidok/boilerplate/JDBC/JDBCConnection.java
@@ -1,4 +1,4 @@
-package io.leedokchidok.boilerplate;
+package io.leedokchidok.boilerplate.JDBC;
 
 import static org.junit.Assert.fail;
 
@@ -13,7 +13,7 @@ import lombok.extern.log4j.Log4j;
  * JDBC(Java Database Connectivity)는 자바에서 데이터베이스에 접속할 수 있도록 하는 자바 API이다
  * */
 @Log4j
-public class JDBCTests {
+public class JDBCConnection {
 
 	static {
 		try {
@@ -27,9 +27,9 @@ public class JDBCTests {
 	public void testConnection() {
 
 		//DB schema
-		String schema	= "DB_Schema";
-		String username	= "ex_username";
-		String password	= "ex_password";
+		String schema	= "DB_Schema";	//스키마
+		String username	= "ex_username";//유저명
+		String password	= "ex_password";//비밀번호
 
 		try (Connection con =
 				DriverManager.getConnection(//mysql 연결 : DB_Schema/ex_username/ex_password
