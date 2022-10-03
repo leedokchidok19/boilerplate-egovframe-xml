65fb33b5814f5857398ec22ced044d4e5406f604
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-10-03 22:43:36 +0900

    init:pom설정 변경 및 HikariCP, spring test 추가 그리고 Test 코드

---
+++ boilerplate-egovframe-xml/src/test/java/io/leedokchidok/boilerplate/persistence/DataSourceTests.java
@@ -0,0 +1,53 @@
+package io.leedokchidok.boilerplate.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+/**
+ * @Class Name	:	DataSourceTests.java
+ * @Description	:	DataSourceTests Class
+ * @Modification	Information
+ * @
+ * @	수정일				수정자					수정내용
+ * @ ---------		--------------------	-------------------------------
+ * @ 2022.10.03			leedokchidok				최초생성, 빈으로 등록된 DataSource를 이용해서 Connection 확인하는 용도
+ *
+ * @author leedokchidok
+ * @since 2022. 07.19
+ * @version 1.0
+ * @see
+ *
+ *  Copyright (C) by MOPAS All right reserved.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/resources/egovframework/spring/context-datasource.xml")
+//Java 설정 사용하는 경우(설정 미구현)
+//@@ContextConfiguration(classes = {RootConfig.class})
+@Log4j
+public class DataSourceTests {
+
+	@Setter(onMethod_= {@Autowired})
+	private DataSource dataSource;
+
+	@Test
+	public void testConnection() {
+
+		try (Connection con = dataSource.getConnection()){
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
